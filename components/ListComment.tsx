70e15838483773204286d0db212e78c44e2ee63c
Đoàn Gia Bảo <giabaod345@gmail.com>
2024-05-01 22:29:14 +0700

    hoan thanh chuc nang binh luan

--- components/ListComment.tsx
+++ components/ListComment.tsx
@@ -1,20 +1,35 @@
 import { instance } from "@/config";
 import { commentStore, userState } from "@/store";
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const ListComment = () => {
+  const { user } = userState();
   const params = useParams();
   const { comments, setCommentInpost } = commentStore();
-  const { user } = userState();
+  const [content, setContent] = useState<string>("");
+  const [idEdit, setIdEdit] = useState("");
   const deleteComment = async (id: any) => {
     const res = await instance.delete(`/comment/delete/?id=${id}`);
     if (res.data.code === 200) {
       setCommentInpost(params.id);
-      toast.success("Xóa thành công");
-    } else {
-      toast.error("Đã xảy ra lỗi");
+      return toast.success("Xóa thành công");
+    }
+  };
+  const editComment = async (cmtId: any) => {
+    if (!content) {
+      return toast.error("Bạn cần nhập nội dung");
+    }
+    const res = await instance.put("/comment/edit", {
+      cmtId,
+      postId: params.id,
+      content,
+    });
+    if (res.data.code === 200) {
+      setCommentInpost(params.id);
+      setIdEdit("");
+      return toast.success("Cập nhật thành công");
     }
   };
   useEffect(() => {
@@ -28,7 +43,7 @@ const ListComment = () => {
           return (
             <div key={item.id}>
               <div className="flex justify-between items-end">
-                <div className="flex space-x-4 items-start">
+                <div className="flex space-x-4 items-start flex-1">
                   <p className="capitalize w-[50px] h-[50px] bg-slate-200  items-center p-3 rounded-full cursor-pointer transition-all text-[#272727 ] text-xl  font-bold">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -46,23 +61,56 @@ const ListComment = () => {
                       <circle cx="12" cy="7" r="4" />
                     </svg>
                   </p>
-                  <div className="space-y-3">
+                  <div className="space-y-3 flex-1">
                     <p className="text-xl">{item?.author.name}</p>
-                    <p className="text-gray-400 text-base">{item.createAt}</p>
-                    <p className="text-xl line-clamp-3">{item.content}</p>
+                    <p className="text-gray-400 text-base">{item?.createAt}</p>
+                    {idEdit === item.id && idEdit ? (
+                      <div className="flex-1 flex-col justify-end">
+                        <textarea
+                          onChange={(e) => setContent(e.target.value)}
+                          className="textarea w-full  h-[200px] textarea-bordered"
+                          placeholder="Nội dung bình luận"
+                        ></textarea>
+                        <div>
+                          <div className="flex space-x-2 items-center">
+                            <button
+                              onClick={() => editComment(item.id)}
+                              className="px-3 py-2 rounded bg-greenTheme text-white"
+                            >
+                              Cập nhật
+                            </button>
+                            <button
+                              onClick={() => setIdEdit("")}
+                              className="px-3 py-2 rounded bg-red-500 text-white"
+                            >
+                              Bỏ chỉnh sửa
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      <p className="text-xl line-clamp-3">{item?.content}</p>
+                    )}
                   </div>
                 </div>
-                <div
-                  className={`action space-x-2 ${item.author.id === user.id ? "" : "hidden"}`}
-                >
-                  <span className="text-greenTheme cursor-pointer">sửa</span>
-                  <span
-                    onClick={() => deleteComment(item.id)}
-                    className="text-red-400 cursor-pointer"
+                {!idEdit && (
+                  <div
+                    className={`action space-x-2 ${item?.author.id === user?.id ? "" : "hidden"}`}
                   >
-                    Xóa
-                  </span>
-                </div>
+                    <span
+                      className="text-greenTheme cursor-pointer"
+                      onClick={() => setIdEdit(item.id)}
+                    >
+                      Sửa
+                    </span>
+                    <span
+                      onClick={() => deleteComment(item.id)}
+                      className="text-red-400 cursor-pointer"
+                    >
+                      Xóa
+                    </span>
+                  </div>
+                )}
               </div>
               <br />
               <hr />
